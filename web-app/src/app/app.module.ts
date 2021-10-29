b42dd60ff946a6cb258406d31b093bd05fd12ae2
Unknown <vingeobrun@googlemail.com>
2021-10-29 20:25:34 +0200

    Google Maps
    Added google maps to the Angular project

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +36,8 @@ import { ExploreComponent } from './explore/explore.component';
     BottomNavModule,
     MatIconModule,
     MatSliderModule,
-    MatToolbarModule
+    MatToolbarModule,
+    GoogleMapsModule
   ],
   providers: [
     Title
--- web-app/src/app/explore/explore.component.ts
+++ web-app/src/app/explore/explore.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
